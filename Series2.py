9a93b17fedbc638500f50ae823680611cee4a1ad
Abayomi <hookes2@yahoo.com>
2021-09-29 13:32:10 -0700

    Deleted TeachingPythonSeries and added Series2

---
+++ Series2.py
@@ -0,0 +1,8 @@
+#Password Checker
+
+user_name = 'Paul'
+password = 'LaughingOutLoud'
+password_length = len(password)
+hash_password = '*' * password_length
+
+print(f'Hey {user_name}, your password {hash_password} is {password_length} characters long!')
